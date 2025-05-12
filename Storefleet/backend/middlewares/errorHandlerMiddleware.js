4559c5bdc1680c5af73881d63eccb72b9198aad9
viveksingh2310 <viveksingh14538@gmail.com>
2025-05-13 01:22:40 +0530

    update errorhandlerMiddleware.js

--- Storefleet/backend/middlewares/errorHandlerMiddleware.js
+++ Storefleet/backend/middlewares/errorHandlerMiddleware.js
@@ -1,15 +1,23 @@
 import { ErrorHandler } from "../utils/errorHandler.js";
 
 export const errorHandlerMiddleware = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err); // Avoid sending headers again if already sent
+  }
+
   err.message = err.message || "Internal server error";
   err.statusCode = err.statusCode || 500;
-  res.status(err.statusCode).json({ success: false, error: err.message });
+
+  res.status(err.statusCode).json({
+    success: false,
+    error: err.message,
+  });
 };
 
-// handling handleUncaughtError  Rejection
+// Handling Uncaught Exception
 export const handleUncaughtError = () => {
   process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err}`);
-    console.log("shutting down server bcz of uncaughtException");
+    console.error(`Uncaught Exception: ${err.message}`);
+    process.exit(1); // Exit the process to avoid undefined behavior
   });
 };
